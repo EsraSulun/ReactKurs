08362b82be410c17c0e47ab7ca07e0092485a33f
Esra Sülün <esrasulun123@gmail.com>
2025-04-18 14:48:50 +0300

    ilk proje (course-app)

---
+++ 7.COURSE-APP/src/Course.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Course({ course }) {
+    const { id, title, description, price, link, image } = course;
+    return (
+        <div className='course'>
+            <div>
+                <img src={image} width={250} height={150} />
+                <h4>{title}</h4>
+                <h5>{description}</h5>
+                <h3>{price}</h3>
+                <h6>{link}</h6>
+            </div>
+        </div>
+    )
+}
+
+export default Course
---
+++ 7.COURSE-APP/src/Data.jsx
@@ -0,0 +1,40 @@
+import Csharp from './images/csharp.png'
+import Java from './images/java.png'
+import Sql from './images/sql.png'
+import Web from './images/web.png'
+
+
+export const courses = [
+    {
+        id: 1244,
+        title: "C# Programlama Dili",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero culpa illo nesciunt sed accusantium in animi libero cupiditate dolorem distinctio.",
+        price: 120,
+        link: "https://www.enesbayram.com/course-detail/csharp-course",
+        image: Csharp
+    },
+    {
+        id: 2345,
+        title: "Java Programlama Dili",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur quia velit ab reprehenderit numquam ratione.",
+        price: 130,
+        link: "https://www.enesbayram.com/course-detail/java-course",
+        image: Java
+    },
+    {
+        id: 5674,
+        title: "SQl Server Eğitimi",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero culpa illo nesciunt sed accusantium in animi libero cupiditate dolorem distinctio.",
+        price: 145,
+        link: "https://www.enesbayram.com/course-detail/csharp-course",
+        image: Sql
+    },
+    {
+        id: 4765,
+        title: "Modern Web Geliştirme Eğitimi",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero culpa illo nesciunt sed accusantium in animi libero cupiditate dolorem distinctio.",
+        price: 120,
+        link: "https://www.enesbayram.com/course-detail/csharp-course",
+        image: Web
+    }
+]
